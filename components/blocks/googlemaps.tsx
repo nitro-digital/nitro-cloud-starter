16c3cd629451f29dfe625c421701b13df3502457
MocsonokyAttila <attila.mocsonoky@mediaworks.hu>
2023-04-20 17:15:13 +0200

    xxxxx

--- components/blocks/googlemaps.tsx
+++ components/blocks/googlemaps.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import type { TinaTemplate } from "tinacms";
 
-export const Googlemaps = ({ data}) => {
+export const Googlemaps =  ({ data, parentField = "" }) => {
   return (
     <>
 <div className="ifream">
